55a3414218cd8b02d3f9ad208e64bee8af0394b2
Mauricio Soria <mauricioutn2017@gmail.com>
2023-10-21 11:41:59 -0300

    Estructura principal

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
